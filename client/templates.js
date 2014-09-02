8fa32db1bae50183bc1aaabce0c7627a7cf044fe
Blair Anderson <blair81@gmail.com>
2014-09-02 09:04:23 -0600

    some cleanup

--- client/templates.js
+++ client/templates.js
@@ -8,3 +8,12 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 
   return "<form>\n  <input type=\"text\" placeholder=\"email\" name=\"email\">\n  <input type=\"text\" placeholder=\"api_key\" name=\"api_key\">\n  <input type=\"submit\" value=\"Go\" class=\"btn btn-default full-width\">\n</form>\n";
   });
+
+this["JST"]["./client/templates/shared/navigation.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<a class=\"logout\" href=\"#logout\">logout</a>\n";
+  });
--- client/views/products/index.js
+++ client/views/products/index.js
@@ -1,13 +1,14 @@
 /** @jsx React.DOM */
 
-
 var React = require('react');
 var _ = require('lodash');
 var List = require('../../component/List');
 
 module.exports = function (opts){
-  var products = _(opts.products.models).map( function(product){
-    return _.pick(product.attributes, "id", "name", "created_at")
-  }).sortBy("id").value();
-  React.renderComponent(<List products={products} />, opts.el);
+  var products = opts.products
+  var productAttrs = _(products.models).map(function(product){
+      return _.pick(product.attributes, "id", "name", "created_at")
+    }).sortBy("created_at").value();
+
+  React.renderComponent(<List products={productAttrs} />, opts.el);
 }
